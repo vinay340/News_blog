c1220e38ca1db543ae3afc51fadc75578ca6d17a
puneet <puneetkumar.chirastahalli@tarento.com>
2017-12-27 18:53:35 +0530

    27-Dec

---
+++ public_view_header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/public_view.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="public_view.php"><img src="assets/images/new_balance.gif" alt="logo" hieght="50px" width="50px"></a>
+            </div>
+            <div>
+            <h2>NEWS BLOG 
+                <i class="fa fa-search right" aria-hidden="true"></i>
+                <input class="searchbox right" placeholder="Search news..."type="text"/>
+            </h2>
+            </div>
+        </div>
+    </nav> 
+</body>
+</html>
