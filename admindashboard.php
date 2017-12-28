5bcd0bdde005f34140104fe0c41bdd234b5bd41b
vinay <vinaydas.chippagiri@tarento.com>
2017-12-28 18:36:46 +0530

    sql conn

--- admindashboard.php
+++ admindashboard.php
@@ -6,32 +6,218 @@
     <title>Admin dashboard</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/admindashboard.css">
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
     <?php
     include("sidebar.php");
+    include("mysql_conn.php");
+     
+$sql = 'SELECT title,description,content,date,id FROM news';  
+$sql1='SELECT id,content FROM comment';
+$retval=mysqli_query($conn, $sql); 
+$retval1=mysqli_query($conn, $sql1);  
+ 
+  //$title = $id=$description=$content=$date="";
+
+   
+
+/* while($row = mysqli_fetch_assoc($retval)){  
+    $description=  $row['description'];
+    $content= $row['content'];              
+      $date=$row['date'];           
+        $id=$row['id'];
+     $title=$row['title'];
+       // "--------------------------------<br>";  
+echo $title."<br>";
+//echo "0 results";  
+} */ 
+
     ?>
-    <div class="container-fluid">
-        <div class="wrapper top">
-       <div class="row ">
-           
-       <div class="col-lg-3 col-md-4">
+  
+
+               <div class="container-fluid">
+                  <div class="wrapper top">
+                       <div class="col-md-12 col-sm-12 col-xs-12 row ">
+                           <div class="col-lg-4 col-md-4 col-sm-6">
                                 <div class="panel"id="panel1"> 
                                     <div class="panel-body">
-                                        <div class="pull-left">
+                                        <div class="pull-left col-md-9 col-sm-6">
                                             <span class="stats-number">1000</span>
                                             <p class="stats-info">Number of views</p>
                                         </div>
-                                        <div class="pull-right">
+                                        <div class="pull-right col-md-3 col-sm-6">
                                             <i class="glyphicon glyphicon-eye-open"></i>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-       </div>
-        </div>
-       
-    </div>
+
+                            <div class="col-lg-4 col-md-4 col-sm-6">
+                                <div class="panel"id="panel1"> 
+                                    <div class="panel-body">
+                                        <div class="pull-left col-md-9 col-sm-6">
+                                            <span class="stats-number">1000</span>
+                                            <p class="stats-info">Number of Posts</p>
+                                        </div>
+                                        <div class="pull-right col-md-3 col-sm-6">
+                                            <i class="fa fa-thumb-tack"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-4 col-md-4 col-sm-6 ">
+                                <div class="panel"id="panel1"> 
+                                    <div class="panel-body">
+                                        <div class="pull-left col-md-9 col-sm-6">
+                                            <span class="stats-number">1000</span>
+                                            <p class="stats-info">Number of Comments</p>
+                                        </div>
+                                        <div class="pull-right col-md-3 col-sm-6">
+                                            <i class="fa fa-comments"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                <!-- second row -->
+                        <div class="col-md-12 col-sm-12 col-xs-12 row ">
+                           <div class="col-lg-4 col-md-4 col-sm-6">
+                                <div class="panel"id="panel1"> 
+                                    <div class="panel-body">
+                                        <div class="pull-left col-md-9 col-sm-6">
+                                            <span class="stats-number">1000</span>
+                                            <p class="stats-info">Number of Admins</p>
+                                        </div>
+                                        <div class="pull-right col-md-3 col-sm-6">
+                                            <i class="glyphicon glyphicon-eye-open"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-4 col-md-4 col-sm-6">
+                                <div class="panel"id="panel1"> 
+                                    <div class="panel-body">
+                                        <div class="pull-left col-md-9 col-sm-6">
+                                            <span class="stats-number">1000</span>
+                                            <p class="stats-info">Number of Authors</p>
+                                        </div>
+                                        <div class="pull-right col-md-3 col-sm-6">
+                                            <i class="fa fa-users"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-4 col-md-4 col-sm-6 ">
+                                <div class="panel"id="panel1"> 
+                                    <div class="panel-body">
+                                        <div class="pull-left col-md-9 col-sm-6">
+                                            <span class="stats-number">1000</span>
+                                            <p class="stats-info">Number of Registrations</p>
+                                        </div>
+                                        <div class="pull-right col-md-3 col-sm-6">
+                                            <i class="fa fa-user-plus"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <!-- 3rd row -->
+                        <div class="row">
+                            <div class="col-sm-12 col-xs-12">   
+                                <div class=" col-md-8 well">
+                                    <fieldset class="col-xs-12">
+                                        <h2>Activities</h2>
+                                        <div class="tabs">
+                                           <div > <ul class="nav nav-pills col-md-12" id="tab" >
+                                                <li class="active col-md-4"><a href="#recent_news" data-toggle="tab">Recently Published</a></li>
+                                                <li class="col-md-4"><a href="#recent_comment" data-toggle="tab">Recently Commented</a></li>
+                                            </ul>
+                                            </div>
+                                            
+                                            <div class="tab-content">
+                                                <div id="recent_news" class="tab-pane fade in active">
+                                                   <?php while($row = mysqli_fetch_assoc($retval)){?>
+                                                        <fieldset class="well" id="element">
+                                                            <div class="row">
+                                                                <div class="col-md-6 col-sm-6 col-xs-8">
+                                                                    <strong><?php echo $row['id'] ?></strong>
+                                                                    </div>
+                                                                        <div class="col-md-6 col-sm-6 col-xs-4 text-right">
+                                                                            <?php echo $row['date'] ?>
+                                                                        </div>
+                                                                    </div>
+                                                                <br>
+                                                            <div class="row">
+                                                                <div class="col-md-2 col-sm-12 col-xs-12">
+                                                                    <img src="assets/images/NB.jpg" class="R_img">
+                                                                </div>
+                                                                <div class="col-md-10 col-sm-12 col-xs-12">
+                                                                    <h4><?php echo $row['title']?></h4>
+                                                                    <p><?php echo $row['description']?>
+                                                                    </p>
+                                                                </div>
+                                                            </div>
+        <!-- ........... -->      <div class="ansbox"></div>
+                                                        </fieldset>
+                                                    <?php } ?>
+                                                </div>
+                                                <div id="recent_comment"class="tab-pane fade ">
+                                                <?php while($row1 = mysqli_fetch_assoc($retval1)){?>
+                                                    <fieldset class="well" id="element">
+                                                            <div class="row">
+                                                                <div class="col-md-6 col-sm-6 col-xs-8">
+                                                                    <strong><?php echo $row1['id']?></strong>
+                                                                    </div>
+                                                                        <div class="col-md-6 col-sm-6 col-xs-4 text-right">
+                                                                        <?php echo 'vinay'?>
+                                                                        </div>
+                                                                    </div>
+                                                                <br>
+                                                            <div class="row">
+                                                                <div class="col-md-2 col-sm-12 col-xs-12">
+                                                                    <h4>Comment by :</h4>
+                                                                </div>
+                                                                <div class="col-md-10 col-sm-12 col-xs-12">
+                                                                    <p><?php echo $row1 ['content']?>
+                                                                    </p>
+                                                                </div>
+                                                            </div>
+                                                        </fieldset>
+                                                    <?php }?>
+                                                  
+                                                </div>
+                                            </div>
+                                        </div>  
+                                    
+                                    </fieldset> 
+                                </div>
+                                <div class= "col-md-4 well">
+                                    <fieldset class="col-xs-12">
+                                    <ul class="list-group col-md-6">
+                                        <li class="list-group-item col-md-6" >All <span class="badge ">12</span></li>
+                                        <li class="list-group-item col-md-6">Pending<span class="badge col-md-2">12</span></li>
+                                        <li class="list-group-item col-md-6">Approved<span class="badge col-md-2">5</span></li>
+                                        <li class="list-group-item  col-md-6">Trash<span class="badge col-md-2">3</span></li>
+                                    </ul>    
+                                    </fieldset>
+
+
+                                </div>
+                            </div>
+                        </div>
+                    <!-- ........................ -->
+                    </div>
+                 </div>
 </body>
 </html>
+<script>
+   for(var i =1; i<= 6; i++){
+    $('#element').append($('<div/>', { id: 'r' + i, 'class' : 'ansbox'}))
+  }
+</script>
---
+++ mysql_conn.php
@@ -0,0 +1,34 @@
+<?php  
+$host = 'localhost:3306';  
+$user = 'root';  
+$pass = 'root';  
+$dbname = 'news_blog';  
+$conn = mysqli_connect($host, $user, $pass,$dbname);  
+if(!$conn){  
+  die('Could not connect: '.mysqli_connect_error());  
+}  
+echo 'Connected successfully<br/>';  
+ 
+//mysqli_close($conn);  
+
+
+?>  
+    <!-- include("mysql_conn.php");
+     
+     $sql = 'SELECT title,description,content,date,id FROM news';  
+     $retval=mysqli_query($conn, $sql);  
+       //$title = $id=$description=$content=$date="";
+     
+        
+     
+     /* while($row = mysqli_fetch_assoc($retval)){  
+         $description=  $row['description'];
+         $content= $row['content'];              
+           $date=$row['date'];           
+             $id=$row['id'];
+          $title=$row['title'];
+     echo $title."<br>";
+     //echo "0 results";  
+     } */ 
+     
+         ?> -->
