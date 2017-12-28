715ab651a150e5dfa93ade11a85d1136d2f9a294
puneet <puneetkumar.chirastahalli@tarento.com>
2017-12-28 18:43:01 +0530

    create news page 28-DEC

---
+++ author_dashboard.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <?php 
+    include('author_header.php');
+    ?>
+    <div class="public_content">
+        <div class="right">
+            <a href="create_news.php"><button type="button" class="btn btn-primary">CREATE NEWS</button></a>
+        </div>
+    </div>
+</body>
+</html>
